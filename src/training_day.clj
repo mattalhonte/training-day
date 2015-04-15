04e811934bc0a5cd7032df59f919bbbdaa2058a4
Matt Alhonte <mattalhonte@gmail.com>
2015-04-14 21:22:15 -0400

    message here

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Takes a number, squares it by itself"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "Takes two numbers, gives us the average"
+  (/ (+ a b) 2))
